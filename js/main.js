29bb29dfbc741a42940f2778c495bfac45117ef7
arsenhalushko <arsen.halushko@icloud.com>
2023-02-01 05:00:49 +0200

    JS

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let hello = 'hello';
+console.log(hello);
+let owu = 'owu';
+console.log(owu);
+let com = 'com';
+console.log(com);
+let ua = 'ua';
+console.log(ua);
+const Number = 1;
+console.log(Number);
+const Number2 = 10;
+console.log(Number2);
+const Number3 = -999;
+console.log(Number3);
+const Number4 = 121;
+console.log(Number4);
+const PI = 3.14;
+console.log(PI);
+const Number5 = 2.7;
+console.log(Number5);
+const Number6 = 16;
+console.log(Number6);
+let t = true;
+console.log(t);
+let f = false;
+console.log(f);
+
+let firstName = 'Arsen';
+let  middleName = 'Romanovich';
+let lastName ='Halushko';
+let person = lastName + ' ' + firstName + ' ' +  middleName ;
+console.log(person);
+let person2 = `${lastName} ${firstName} ${middleName}`;
+console.log(person2);
+
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+
+
+
+
+
